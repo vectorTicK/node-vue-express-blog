c710fe11117d35b28bc71a106c579c317a5708ec
tick <tk1011@qq.com>
2019-08-06 20:36:07 +0800

    ..

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,11 @@
+// module.exports = {
+//     outputDir: '../server/web',
+//     publicPath: process.env.NODE_ENV === 'production' ?
+//         '/web/' :
+//         '/'
+// }
 module.exports = {
     outputDir: '../server/web',
     publicPath: process.env.NODE_ENV === 'production' ?
-        '/web/' :
-        '/'
+        '/' : '/'
 }
